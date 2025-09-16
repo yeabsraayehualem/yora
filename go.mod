976af4e4f517e5761cf347469bc076275727d7b8
Yeab <yeabsraayehualem@gmail.com>
2025-09-16 14:42:51 +0300

    changed module name

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module yeabsraayehualem/yora
+module github.com/yeabsraayehualem/yora
 
 go 1.24.6
-
-require github.com/hobord/routegroup v0.0.1 // indirect
---
+++ routes.go
@@ -0,0 +1,10 @@
+
+package main;
+
+import "net/http"
+func RegisterRoutes(mux *http.ServeMux) {
+// mux.Handle("/blog/", http.StripPrefix("/blog", http.HandlerFunc(blog.Router)))
+
+
+}
+	
